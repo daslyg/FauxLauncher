0fa7a8b7eea462a76fe283c31203694dffaa58b1
BuildTools <unconfigured@null.spigotmc.org>
2024-05-19 20:30:57 -0700

    INCOMPREHENSIBLE, HAVE A NICE DAY

--- FauxLauncher/FauxLauncher.cpp
+++ FauxLauncher/FauxLauncher.cpp
@@ -30,10 +30,13 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// Function prototype for ExtractResource
+bool ExtractResource(const HMODULE hModule, const UINT resourceID, const LPCSTR outputFilename);
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -48,7 +51,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     fs::path executablePath(buffer); // Convert directly to fs::path
 
     // Output the path of the executable into a output.txt in outdir
-    fs::path outputPath ="executablePath.txt";
+    fs::path outputPath = "executablePath.txt";
     std::ofstream outputFile(outputPath);
     if (outputFile.is_open()) {
         outputFile << executablePath;
@@ -59,38 +62,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         std::cerr << "Unable to open output.txt for writing." << std::endl;
     }
 
-    bool ExtractResource(const HMODULE hModule, const UINT resourceID, const LPCSTR outputFilename) {
-        HRSRC hResource = FindResource(hModule, MAKEINTRESOURCE(resourceID), RT_RCDATA);
-        if (hResource == NULL) {
-            return false;
-        }
-
-        HGLOBAL hLoadedResource = LoadResource(hModule, hResource);
-        if (hLoadedResource == NULL) {
-            return false;
-        }
-
-        LPVOID lpResourceData = LockResource(hLoadedResource);
-        if (lpResourceData == NULL) {
-            return false;
-        }
-
-        DWORD dwResourceSize = SizeofResource(hModule, hResource);
-        std::ofstream outputFile(outputFilename, std::ios::binary);
-        outputFile.write(reinterpret_cast<const char*>(lpResourceData), dwResourceSize);
-        outputFile.close();
-
-        return true;
-    }
-
-        // Replace 'YOUR_RESOURCE_ID' with the actual resource ID
-        // Replace 'YOUR_OUTPUT_FILENAME' with the desired output file name
-    if (ExtractResource(hModule, 255, "Minecraft.lnk")) {
+    // Replace 'YOUR_RESOURCE_ID' with the actual resource ID
+    // Replace 'YOUR_OUTPUT_FILENAME' with the desired output file name
+    if (ExtractResource(hInstance, 255, "Minecraft.lnk")) {
         // Resource extraction succeeded
     }
     else {
         // Resource extraction failed
-    };
+    }
 
     return (INT_PTR)FALSE;
+}
+
+// Definition of ExtractResource
+bool ExtractResource(const HMODULE hModule, const UINT resourceID, const LPCSTR outputFilename) {
+    HRSRC hResource = FindResource(hModule, MAKEINTRESOURCE(resourceID), RT_RCDATA);
+    if (hResource == NULL) {
+        return false;
+    }
+
+    HGLOBAL hLoadedResource = LoadResource(hModule, hResource);
+    if (hLoadedResource == NULL) {
+        return false;
+    }
+
+    LPVOID lpResourceData = LockResource(hLoadedResource);
+    if (lpResourceData == NULL) {
+        return false;
+    }
+
+    DWORD dwResourceSize = SizeofResource(hModule, hResource);
+    std::ofstream outputFile(outputFilename, std::ios::binary);
+    outputFile.write(reinterpret_cast<const char*>(lpResourceData), dwResourceSize);
+    outputFile.close();
+
+    return true;
 }
